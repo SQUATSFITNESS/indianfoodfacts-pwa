e49a10b80c44f39076e759d79f00aca726a9a541
Souvik Basu <souvikbasu@gmail.com>
2018-12-19 11:55:43 -0800

    add port number in server running message

--- server.js
+++ server.js
@@ -14,5 +14,5 @@ const port = process.env.PORT || 3001;
 app.set('port', port);
 
 const server = http.createServer(app);
-server.listen(port, () => console.log('Server Running!!'));
+server.listen(port, () => console.log('Server Running at port: ' + port));
 
